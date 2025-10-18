aa6e100c60862618d4dc97172f777e39557a7a7f
Khoa1410 <vtrankhoa1410@gmail.com>
2025-10-18 10:35:02 +0700

    basic config for server

---
+++ Running/dstdedicated1.sh
@@ -0,0 +1,2 @@
+cd ~/.steam/steam/steamapps/common/Don\'t\ Starve\ Together\ Dedicated\ Server/bin
+./dontstarve_dedicated_server_nullrenderer -cluster cluster_name -shard Master
---
+++ world_config/Master/modoverrides.lua
@@ -0,0 +1,118 @@
+return {
+  ["workshop-1111658995"]={ configuration_options={  }, enabled=true },
+  ["workshop-1185229307"]={
+    configuration_options={
+      CAMERA=true,
+      CAPTURE=false,
+      DAMAGE_NUMBERS=true,
+      DAMAGE_RESISTANCE=true,
+      FRAME_PHASES=true,
+      GLOBAL=false,
+      GLOBAL_NUMBERS=false,
+      HEADER_CLIENT=false,
+      HEADER_SERVER=false,
+      HORIZONTAL_OFFSET=0,
+      TAG="EPIC",
+      WETNESS_METER=false 
+    },
+    enabled=true 
+  },
+  ["workshop-1207269058"]={ configuration_options={  }, enabled=true },
+  ["workshop-1378549454"]={
+    configuration_options={
+      ["MemSpikeFix:"]=false,
+      MemSpikeFixmaster_override=true,
+      ["MemSpikeFixworkshop-1079538195"]="default",
+      ["MemSpikeFixworkshop-1111658995"]="default",
+      ["MemSpikeFixworkshop-1120124958"]="default",
+      ["MemSpikeFixworkshop-1185229307"]="default",
+      ["MemSpikeFixworkshop-1207269058"]="default",
+      ["MemSpikeFixworkshop-1686705509"]="default",
+      ["MemSpikeFixworkshop-1852257480"]="default",
+      ["MemSpikeFixworkshop-2078243581"]="default",
+      ["MemSpikeFixworkshop-2287303119"]="default",
+      ["MemSpikeFixworkshop-2302837868"]="default",
+      ["MemSpikeFixworkshop-3147439639"]="default",
+      ["MemSpikeFixworkshop-345692228"]="default",
+      ["MemSpikeFixworkshop-347079953"]="default",
+      ["MemSpikeFixworkshop-351325790"]="default",
+      ["MemSpikeFixworkshop-356930882"]="default",
+      ["MemSpikeFixworkshop-362175979"]="default",
+      ["MemSpikeFixworkshop-365119238"]="default",
+      ["MemSpikeFixworkshop-374550642"]="default",
+      ["MemSpikeFixworkshop-375850593"]="default",
+      ["MemSpikeFixworkshop-375859599"]="default",
+      ["MemSpikeFixworkshop-376333686"]="default",
+      ["MemSpikeFixworkshop-378160973"]="default",
+      ["MemSpikeFixworkshop-466732225"]="default",
+      ["MemSpikeFixworkshop-478005098"]="default",
+      ["MemSpikeFixworkshop-569043634"]="default",
+      ["MemSpikeFixworkshop-623749604"]="default",
+      ["MemSpikeFixworkshop-661253977"]="default",
+      ["MemSpikeFixworkshop-666155465"]="default",
+      ["MemSpikeFixworkshop-727774324"]="default",
+      ["MemSpikeFixworkshop-758532836"]="default",
+      ["MemSpikeFixworkshop-836583293"]="default",
+      ["MemSpikeFixworkshop-956206484"]="default",
+      craftinghighlight=false 
+    },
+    enabled=true 
+  },
+  ["workshop-1852257480"]={
+    configuration_options={ horizontal=0, info=false, position="bottom", vertical="auto" },
+    enabled=true 
+  },
+  ["workshop-2078243581"]={
+    configuration_options={ Blue=0, Display="target", Green=0, Projectile=true, Red=1, Type="hit" },
+    enabled=true 
+  },
+  ["workshop-3147439639"]={ configuration_options={ DEBUG=0, enable_debug=false }, enabled=true },
+  ["workshop-347079953"]={
+    configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-356930882"]={
+    configuration_options={ uses=10000000, uses2=10000000, uses3=10000000 },
+    enabled=true 
+  },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-374550642"]={
+    configuration_options={
+      STACK_SIZE_LARGEITEM=99,
+      STACK_SIZE_MEDITEM=99,
+      STACK_SIZE_PELLET=120,
+      STACK_SIZE_SMALLITEM=99,
+      STACK_SIZE_TINYITEM=99 
+    },
+    enabled=true 
+  },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-378160973"]={
+    configuration_options={
+      ENABLEPINGS=true,
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-466732225"]={ configuration_options={  }, enabled=true },
+  ["workshop-661253977"]={
+    configuration_options={ amudiao=true, baodiao=1, kong=0, nillots=0, rendiao=2, zbdiao=true },
+    enabled=true 
+  },
+  ["workshop-758532836"]={
+    configuration_options={
+      AUTOPAUSECONSOLE=false,
+      AUTOPAUSEMAP=false,
+      AUTOPAUSESINGLEPLAYER=true,
+      ENABLECLIENTPAUSING=false,
+      ENABLEHOTKEY=false,
+      KEYBOARDTOGGLEKEY="P" 
+    },
+    enabled=true 
+  } 
+}
